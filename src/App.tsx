a8d387b175faf39bc0e795e22cde1d11b3082076
gastonivanfrasca <gastonivanfrasca@hotmail.com>
2025-02-25 00:45:52 -0300

    fix: clean up commented code and improve formatting in App component

--- src/App.tsx
+++ src/App.tsx
@@ -78,17 +78,20 @@ function App() {
           >
             <div className="flex flex-col gap-2 rounded-sm dark:text-gray-200  grow break-words max-w-full items-start pb-4">
               <div className="flex flex-row gap-2 items-start">
+                {/*
                 <div className="flex items-center rounded-full pt-1 max-w-[20px]">
                   <img
                     src={getImageBySourceID(item.source || "")}
                     alt={item.rssName || ""}
                     className="rounded-full w-[200px] h-[20px]"
                   />
-                </div>
+                </div> */}
                 <p className="font-semibold text-lg">{item.title}</p>
               </div>
               <div className="flex flex-row gap-2 w-full justify-end items-end mt-2">
-                <p className="text-xs overflow-ellipsis whitespace-nowrap">{getSourceName(item.source)} | </p>
+                <p className="text-xs overflow-ellipsis whitespace-nowrap">
+                  {getSourceName(item.source)} |{" "}
+                </p>
                 {item.pubDate && (
                   <p className="text-xs self-end text-right whitespace-nowrap">
                     {formatPubDate(item.pubDate)}
